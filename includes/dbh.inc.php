90c2e7bfadeac7af1c82ac196deae3204ce1b779
Anas-Diabi <33428345+Hackermans007@users.noreply.github.com>
2024-01-09 22:54:32 +0100

    database
    pdo database conection

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,13 +1,15 @@
 <?php
 
 $host = 'localhost';
-$dbname = 'anas_php_training';
+$dbname = 'wiki';
 $dbusername = 'root';
 $dbpassword = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Conection Failed: " . $e->getMessage());
+    die("Connection Failed: " . $e->getMessage());
 }
+
+

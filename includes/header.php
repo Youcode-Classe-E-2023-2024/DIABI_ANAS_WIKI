6a7feb6ee09512a6daef3223137967a3a06cdcd5
Anas-Diabi <33428345+Hackermans007@users.noreply.github.com>
2024-01-09 22:40:43 +0100

    header/navbar/sidebar_Includes

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'includes/config_session.inc.php';
+require_once 'view\signup_view.inc.php';
+require_once 'view\login_view.inc.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-dfJvW1RlZj5FpxJ3z9+uL4P6blgM5ZPaUwT4uFR16n1UvA6HgPQ9CExlJEPi2Jmw" crossorigin="anonymous">
+    <title>Login & Registration</title>
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <!-- style -->
+    <link rel="stylesheet" href="style/style.css">
+</head>

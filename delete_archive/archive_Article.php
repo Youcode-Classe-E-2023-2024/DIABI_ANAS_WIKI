b84d1250e9d3df49505aaf17187f424fc7d8d83d
Anas-Diabi <33428345+Hackermans007@users.noreply.github.com>
2024-01-11 19:24:11 +0100

    archive/publier/supprimer un article

--- delete_archive/archive_Article.php
+++ delete_archive/archive_Article.php
@@ -1,25 +1,30 @@
 <?php
    
    include("../includes/dbh.inc.php");
+   include("../model/dashboard_model.php");
 
-   if(isset($_GET["arch"])){
+   if (isset($_GET["id"]) && isset( $_GET["newstat"])) {
 
-    $id = $_GET["arch"];
-    $query = "UPDATE articles SET status = 'archived' WHERE id = :id;";
-    $stmt = $pdo->prepare($query);
-    $stmt->bindParam(":id", $id);
-    $stmt->execute();
+      $id = $_GET["id"];
+      $stat = $_GET["newstat"];
+      
+      
+      toggleArticleStatus($pdo, $id, $stat);
 
-    header("location: ../Articles.php");
-    
 
-   }elseif(isset($_GET["pub"])){
+  } elseif (isset($_GET["pub"])) {
       $id = $_GET["pub"];
-    $query = "UPDATE articles SET status = 'public' WHERE id = :id;";
-    $stmt = $pdo->prepare($query);
-    $stmt->bindParam(":id", $id);
-    $stmt->execute();
+      toggleArticleStatus($pdo, $id, $stat);
+  } elseif (isset($_GET["del"])) {
+  
+   $id = $_GET["del"];
+   $query = "DELETE from articles WHERE id = :id;";
+   $stmt = $pdo->prepare($query);
+   $stmt->bindParam(":id", $id);
+   $stmt->execute();
 
-    header("location: ../Articles.php");
-    
-   }
+   header("location: ../articles.php");
+   
+
+
+}  
